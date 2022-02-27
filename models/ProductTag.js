e35d2bf25ddcebcc88ea4b7fc81cff0cfe0572b2
Kate Johnson <johnson.kat.g@gmail.com>
2022-02-26 22:29:07 -0500

    set up project

---
+++ models/ProductTag.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+    {
+        //     *id
+        // Integer
+        // Doesn't allow null values
+        // Set as primary key
+        // Uses auto increment
+        // *product_id
+        // Integer
+        // References the product model's id
+        // *tag_id
+        // Integer
+        // References the tag model's id
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'product_tag'
+    }
+);
+
+module.exports = ProductTag;
--- models/index.js
+++ models/index.js
@@ -4,11 +4,13 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Product belongs to Category, as a category can have multiple products but a product can only belong to one category.
 
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
+// Product belongs to many Tag models. Using the ProductTag through model, 
+// allow products to have multiple tags and tags to have many products.
 
 // Tags belongToMany Products (through ProductTag)
 
