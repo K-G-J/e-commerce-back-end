2232925ecd21ba277fd4f151daa5cc949908ec85
Kate Johnson <johnson.kat.g@gmail.com>
2022-02-27 03:26:09 -0500

    made models and associations

--- models/index.js
+++ models/index.js
@@ -4,15 +4,20 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Product belongs to Category, as a category can have multiple products but a product can only belong to one category.
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-// Product belongs to many Tag models. Using the ProductTag through model, 
-// allow products to have multiple tags and tags to have many products.
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+})
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+})
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+})
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,42 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+// GET all categories 
+router.get('/', async (req, res) => {
+    try {
+        const dbCategoryData = await Category.findAll({
+            include: [ { model: Product, attributes: [ 'product_name', 'price', 'stock' ] } ]
+        });
+        res.json(dbCategoryData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
-
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+// GET a single category 
+router.get('/:id', async (req, res) => {
+    try {
+      const dbCategoryData = await Category.findAll({
+          where: { id: req.params.id },
+          include: [ { model: Product, attributes: ['product_name', 'price', 'stock'] } ]
+      });
+        res.json(dbCategoryData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+    // create a new category
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+    // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+    // delete a category by its `id` value
 });
 
 module.exports = router;
